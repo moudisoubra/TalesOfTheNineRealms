225d710374a5ffd4417c08bd985cd81592e6a2ae
moudisoubra <moudisoubra001@gmail.com>
2020-04-07 21:49:00 +0400

    Dice make roll be crazy but look good very fun

--- Assets/Soubra/Scripts/NewestPathfinding/Attacks/Player/OdinWarriorClass.cs
+++ Assets/Soubra/Scripts/NewestPathfinding/Attacks/Player/OdinWarriorClass.cs
@@ -14,8 +14,6 @@ public class OdinWarriorClass : CellPositions
 
             AddNode(n, 0, 0);
             AddNode(n, 0, 1);
-            AddNode(n, 0, 2);
-            AddNode(n, 0, 3);
         }
         if (direction == Direction.Down)
         {
@@ -23,8 +21,6 @@ public class OdinWarriorClass : CellPositions
 
             AddNode(n, 0, 0);
             AddNode(n, 0, -1);
-            AddNode(n, 0, -2);
-            AddNode(n, 0, -3);
         }
         if (direction == Direction.Left)
         {
@@ -32,8 +28,6 @@ public class OdinWarriorClass : CellPositions
 
             AddNode(n, 0, 0);
             AddNode(n, -1, 0);
-            AddNode(n, -2, 0);
-            AddNode(n, -3, 0);
         }
         if (direction == Direction.Right)
         {
@@ -41,8 +35,6 @@ public class OdinWarriorClass : CellPositions
 
             AddNode(n, 0, 0);
             AddNode(n, 1, 0);
-            AddNode(n, 2, 0);
-            AddNode(n, 3, 0);
         }
     }
 
