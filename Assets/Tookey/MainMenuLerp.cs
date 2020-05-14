41792fa57d470989b4a34bc695f52376b7dbe102
papaooke <anna.tookey@hotmail.com>
2020-05-14 21:34:52 +0400

    Main menu!!

---
+++ Assets/Tookey/MainMenuLerp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuLerp : MonoBehaviour
+{
+    public GameObject[] cameraPos;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
